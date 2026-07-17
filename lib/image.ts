a17b96977e281bbae258a5730069bbcbc02624e7
suzeyy <sujanapyakurel2000@gmail.com>
2026-07-17 11:28:17 +0545

    fix: define variables

--- lib/image.ts
+++ lib/image.ts
@@ -1,10 +1,10 @@
-import createImageUrlBuilder from '@sanity/image-url'
+import createImageUrlBuilder, { SanityImageSource } from '@sanity/image-url'
 
-import { dataset, projectId } from '../env'
+import { dataset, projectId } from '../sanity/env'
 
 // https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({ projectId, dataset })
 
-export const urlFor = (source) => {
+export const urlFor = (source: SanityImageSource) => {
   return builder.image(source)
 }
